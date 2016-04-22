6676c182c32201c9822f892b2b5856b0977ad333
Janelle Hinds <janelle@admins-MacBook-Pro.local>
2016-04-22 10:30:20 -0400

    fixed colours , data and labels

--- app/src/main/java/com/example/janelle/myapplicationsample/DummyData.java
+++ app/src/main/java/com/example/janelle/myapplicationsample/DummyData.java
@@ -27,10 +27,10 @@ public class DummyData {
 
     public static ArrayList<PieData> generateDummyPieData() {
         ArrayList<PieData> pieDatas = new ArrayList<>();
-        pieDatas.add(new PieData("100", "#FF0000", "#FF3300", "Red"));
-        pieDatas.add(new PieData("100", "#EEEE00", "#EEEE70", "Yellow"));
-        pieDatas.add(new PieData("100", "#FFFFFF", "#FF99FF", "White"));
-        pieDatas.add(new PieData("100", "#000000", "#003300", "Black"));
+        pieDatas.add(new PieData("50", "#FF0000", "#FF3300", "Mental"));
+        pieDatas.add(new PieData("70", "#EEEE00", "#EEEE70", "Physical"));
+        pieDatas.add(new PieData("60", "#D6D6D6", "#FF99FF", "Spirtual"));
+        pieDatas.add(new PieData("80", "#F2F2F2", "#003300", "Emotional"));
         return pieDatas;
     }
 
@@ -44,15 +44,16 @@ public class DummyData {
         List<ChartDataSet> dataSets = new ArrayList<>();
         List<Double> data = new ArrayList<>();
 
-        data.add(3000D);
-        data.add(3000D);
-        data.add(2300D);
+        data.add(50D);
+        data.add(70D);
+        data.add(60D);
+        data.add(80D);
 
         dataSets.add(new ChartDataSet(
                 "Data set 1",
                 "rgba(220,220,220,0.2)",
                 "rgba(220,220,220,1)",
-                "rgba(220,220,220,1)",
+                "rgba(10,50,245,1)",
                 "#fff",
                 "#fff",
                 "rgba(220,220,220,1)", data));
